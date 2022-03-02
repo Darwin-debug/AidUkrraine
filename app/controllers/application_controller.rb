d9f9ea90f1c49db71db21ebc6aa2df0aaf697af0
Anatoliy Potapchuk <potapchukanatoliy@gmail.com>
2022-03-02 01:38:45 +0000

    Fix path redirect after session status change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    def after_sign_in_path_for(resource)
+        aid_proposals_path
+    end
+
+    def after_sign_up_path_for(resource)
+        aid_proposals_path
+    end
+
+    def after_sign_out_path_for(resource_or_scope)
+        root_url
+    end
 end

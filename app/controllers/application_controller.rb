0ea3b5a9b1f8c507ee33f8e9b67c6b334b6084eb
Anatoliy Potapchuk <potapchukanatoliy@gmail.com>
2022-03-02 15:04:28 +0000

    Change redirect after sign in to new form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource)
-        aid_proposals_path
+        new_aid_proposal_path
     end
 
     def after_sign_up_path_for(resource)
-        aid_proposals_path
+        new_aid_proposal_path
     end
 
     def after_sign_out_path_for(resource_or_scope)

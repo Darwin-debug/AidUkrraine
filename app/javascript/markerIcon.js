8c5c0eec0ea331754de608db800334444f1991b7
BM086325 <Borys_Mohyla@epam.com>
2022-03-04 14:17:25 +0200

    remove console.log

--- app/javascript/markerIcon.js
+++ app/javascript/markerIcon.js
@@ -1,6 +1,5 @@
 import L from 'leaflet';
 
-console.log(L.Icon.Default.prototype.options)
 const icon = L.icon({
   ...L.Icon.Default.prototype.options,
   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),

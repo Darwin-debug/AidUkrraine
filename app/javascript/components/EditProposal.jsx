e788ad2daa7a05e61d7475a0a5ad35dfde064c6f
BM086325 <Borys_Mohyla@epam.com>
2022-03-04 09:51:24 +0200

    add def input value

--- app/javascript/components/EditProposal.jsx
+++ app/javascript/components/EditProposal.jsx
@@ -74,7 +74,7 @@ const EditProposal = ({ action, method, id }) => {
                         name="aid_proposal[full_name]"
                         id="full_name"
                         type="text"
-                        value={aidProposal.full_name}
+                        value={aidProposal.full_name || ''}
                     />
                     <Errors errors={errors.full_name} />
                 </div>
@@ -130,7 +130,7 @@ const EditProposal = ({ action, method, id }) => {
                         name="aid_proposal[country]"
                         id="country"
                         type="text"
-                        value={aidProposal.country}
+                        value={aidProposal.country || ''}
                     />
                     <Errors errors={errors.country} />
                 </div>
@@ -145,7 +145,7 @@ const EditProposal = ({ action, method, id }) => {
                         onChange={onChange}
                         name="aid_proposal[city]"
                         id="city" type="text"
-                        value={aidProposal.city}
+                        value={aidProposal.city || ''}
                     />
                     <Errors errors={errors.city} />
                 </div>
@@ -153,28 +153,28 @@ const EditProposal = ({ action, method, id }) => {
                     <label htmlFor="description">
                         Description of the items needed / Опис необхідних предметів
                     </label>
-                    <textarea form="editAidProposal" className="form-control" onChange={onChange} name="aid_proposal[description]" id="description" value={aidProposal.description} />
+                    <textarea form="editAidProposal" className="form-control" onChange={onChange} name="aid_proposal[description]" id="description" value={aidProposal.description || ''} />
                     <Errors errors={errors.description} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="url" className="form-label required">
                         Additional URL / Додаткове посилання
                     </label>
-                    <input form="editAidProposal" className="form-control" onChange={onChange} name="aid_proposal[url]" id="url" type="url" value={aidProposal.url} />
+                    <input form="editAidProposal" className="form-control" onChange={onChange} name="aid_proposal[url]" id="url" type="url" value={aidProposal.url || ''} />
                     <Errors errors={errors.url} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="date" className="form-label required">
                         Departure date / Дата від'їзду
                     </label>
-                    <input form="editAidProposal" className="form-control" onChange={onChange} name="aid_proposal[date]" id="date" type="date" value={aidProposal.date} />
+                    <input form="editAidProposal" className="form-control" onChange={onChange} name="aid_proposal[date]" id="date" type="date" value={aidProposal.date || ''} />
                     <Errors errors={errors.date} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="contact" className="form-label required">
                         Contact information / Контактна інформація
                     </label>
-                    <textarea form="editAidProposal" className="form-control" onChange={onChange} name="aid_proposal[contact]" id="contact" value={aidProposal.contact} />
+                    <textarea form="editAidProposal" className="form-control" onChange={onChange} name="aid_proposal[contact]" id="contact" value={aidProposal.contact || ''} />
                     <Errors errors={errors.contact} />
                 </div>
                 <div className="mb-3">

7e582b0028a8cb413a9fa8f4417818f306e37859
BM086325 <Borys_Mohyla@epam.com>
2022-03-05 15:10:06 +0200

    translate show

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-    config.site_key  = Rails.application.credentials.recaptcha.public
-    config.secret_key = Rails.application.credentials.recaptcha.private
+    config.site_key  = ''
+    config.secret_key = ''
 end

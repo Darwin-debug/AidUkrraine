0a71dd93f74453d4f232f9c091860e30c0ce57a4
BM086325 <Borys_Mohyla@epam.com>
2022-03-05 17:44:40 +0200

    add link to map

--- app/javascript/components/aid-proposals/ProposalsList.jsx
+++ app/javascript/components/aid-proposals/ProposalsList.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useIntl } from 'react-intl';
 import { Map, Marker, TileLayer, ZoomControl, Popup } from 'react-leaflet';
 
 import Proposal from './Proposal';
@@ -8,58 +9,68 @@ import markerIcon from '../../markerIcon';
 import 'leaflet/dist/leaflet.css'
 import './search.css'
 
-const ProposalsList = ({ proposals, loading }) => (
-    <div>
-        <div className="card mb-3">
-            <Map
-                center={[51.42169068153824, 18.821976536948544]}
-                zoom={4}
-                scrollWheelZoom={false}
-                style={{ width: '100%', height: '300px' }}
-                zoomControl={false}>
-                <ZoomControl position="bottomright" />
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                {
-                    proposals
-                        .filter(proposal => proposal.lat)
-                        .map(proposal => (
-                            <Marker
-                                key={proposal.id}
-                                position={proposal}
-                                icon={markerIcon}
-                            >
-                                <Popup>
-                                    <div className="card">
-                                        <div className="card-body">
-                                            <h5 className="card-title"> {proposal.country}, {proposal.city} </h5>
-                                            <h6 className="card-subtitle mb-2 text-muted"> {proposal.date} - <b> {proposal.full_name}</b></h6>
+const ProposalsList = ({ proposals, loading }) => {
+    const intl = useIntl();
+    return (
+        <div>
+            <div className="card mb-3">
+                <Map
+                    center={[51.42169068153824, 18.821976536948544]}
+                    zoom={4}
+                    scrollWheelZoom={false}
+                    style={{ width: '100%', height: '300px' }}
+                    zoomControl={false}>
+                    <ZoomControl position="bottomright" />
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    {
+                        proposals
+                            .filter(proposal => proposal.lat)
+                            .map(proposal => (
+                                <Marker
+                                    key={proposal.id}
+                                    position={proposal}
+                                    icon={markerIcon}
+                                >
+                                    <Popup>
+                                        <div className="card">
+                                            <div className="card-body">
+                                                <h5 className="card-title"> {proposal.country}, {proposal.city} </h5>
+                                                <h6 className="card-subtitle mb-2 text-muted"> {proposal.date} - <b> {proposal.full_name}</b></h6>
+                                                <div>
+                                                    <a className="text-dark" href={`aid_proposals/${proposal.id}`} >
+                                                        {intl.formatMessage({
+                                                            id: 'aid_proposals.index.proposal_details'
+                                                        })}
+                                                    </a>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                </Popup>
-                            </Marker>
-                        ))
-                }
-                
-            </Map>
-        </div>
-        {
-            loading ? (
-                <div className="d-flex justify-content-center">
-                    <div className="spinner-border" role="status">
-                        <span className="visually-hidden">Loading...</span>
+                                    </Popup>
+                                </Marker>
+                            ))
+                    }
+                    
+                </Map>
+            </div>
+            {
+                loading ? (
+                    <div className="d-flex justify-content-center">
+                        <div className="spinner-border" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
                     </div>
-                </div>
-            ) : proposals.map(proposal => (
-                <Proposal
-                    proposal={proposal}
-                    key={proposal.id}
-                />
-            ))
-        }
-    </div>
-);
+                ) : proposals.map(proposal => (
+                    <Proposal
+                        proposal={proposal}
+                        key={proposal.id}
+                    />
+                ))
+            }
+        </div>
+    );
+}
 
 export default ProposalsList;

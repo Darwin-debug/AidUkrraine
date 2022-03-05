5533f618491bb19ab13fc67fa87568c0dc735442
BM086325 <Borys_Mohyla@epam.com>
2022-03-05 19:15:59 +0200

    fix dateUpdate

--- app/javascript/components/edit-proposal/EditProposal.jsx
+++ app/javascript/components/edit-proposal/EditProposal.jsx
@@ -5,15 +5,15 @@ import MapInput from './MapInput';
 
 import useEditAidProposal from './hooks/useEditAidProposal';
 
-const DateSelector = ({date, error, onChangeOuter}) => {
+const DateSelector = ({date, error, changeDate}) => {
     const intl = useIntl();
     const [ regularityState, setRegularityState] = useState( date ? "specific" : "regular"); 
 
     const onChangeRegularity = (e) => {
         setRegularityState(e.target.value);
-        // Somehow reset the date element.
+        if (regularityState === 'specific') changeDate('');
     }
-    const onChange = (e) => onChangeOuter(e);
+    const onChange = (e) => changeDate(e.target.value);
   
     return (
         <>
@@ -61,6 +61,13 @@ const EditProposal = ({ action, method, id }) => {
         setAidProposal({
             ...aidProposal,
             [e.target.id]: e.target.value,
+        });
+    }
+
+    const changeDate = (value) => {
+        setAidProposal({
+            ...aidProposal,
+            date: value,
         });
     }
 
@@ -146,7 +153,7 @@ const EditProposal = ({ action, method, id }) => {
                     <input form="editAidProposal" className="form-control" onChange={onChange} name="aid_proposal[url]" id="url" type="url" value={aidProposal.url || ''} />
                     <Errors errors={errors.url} />
                 </div>
-                <DateSelector date={aidProposal.date} error={errors.date} onChangeOuter={onChange} />
+                <DateSelector date={aidProposal.date} error={errors.date} changeDate={changeDate} />
                 <div className="mb-3">
                     <label htmlFor="contact" className="form-label required">
                         {intl.formatMessage({

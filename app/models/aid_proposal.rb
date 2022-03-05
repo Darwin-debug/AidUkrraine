7d2d26f16f0ea227829d6c25cef56ba991bb62e4
Anatoliy Potapchuk <potapchukanatoliy@gmail.com>
2022-03-05 18:18:03 +0000

    Make date not required

--- app/models/aid_proposal.rb
+++ app/models/aid_proposal.rb
@@ -2,7 +2,6 @@ class AidProposal < ApplicationRecord
     validates :country, :presence => true
     validates :city, :presence => true
     validates :full_name, :presence => true
-    validates :date, :presence => true
     validates :contact, :presence => true
     validates :description, :presence => true
     validates :lat, :presence => true
